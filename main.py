199efdc87a98740eaece5a58b38e0e09656cb2f3
Akeksandr <sash.ban2013@yandex.ru>
2024-10-21 21:42:05 +0300

    assd

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"msg": "Hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run("main.app", host="0.0.0.0", port=8000)
